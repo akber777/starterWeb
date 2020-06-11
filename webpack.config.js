98b673b1d1da9b0480e3e85f6df7beb47c3ded0e
Akber Akhmedzadeh <akber_akhmedzadeh@mail.ru>
2020-06-11 17:13:18 +0400

    concat update

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const fileLoader = require('file-loader');
 const CopyPlugin = require('copy-webpack-plugin');
+const htmlLoader = require('html-loader');
+const ConcatPlugin = require('webpack-concat-plugin');
+
 
 
 //bu hissədə yeni yaradılan html fayllarını qeyd edirik
@@ -30,6 +34,14 @@ module.exports = {
             patterns: [
                 { from: 'src/', to: path.join(__dirname, 'build') }
             ],
+        }),
+        new ConcatPlugin({
+            uglify: false,
+            useHash: false,
+            sourceMap: false,
+            name: 'all',
+            fileName: 'js/all.js',
+            filesToConcat: ['./src/js/**', './src/js/**'],
         })
     ],
     module: {
@@ -39,6 +51,14 @@ module.exports = {
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
+        ]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
         ]
     }
 }
